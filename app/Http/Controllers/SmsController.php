d9896793d5d496f918913268f734674e6db86f36
BIGKINGS10 <adventbigkings@gmail.com>
2023-04-20 10:54:31 +0100

    updated SmsController with sendsms function

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
+use Twilio\Rest\Client;
 use Illuminate\Http\Request;
 
 class SmsController extends Controller
@@ -14,5 +16,19 @@ class SmsController extends Controller
     public function send_sms(Request $request){
         $receiver_number = $request->phonenumber;
         $message = $request->message;
+
+        try{
+            $account_sid = getenv("TWILIO_SID");
+            $auth_token = getenv("TWILIO_TOKEN");
+            $twilio_number = getenv("TWILIO_FROM");
+            $client = new Client($account_sid, $auth_token);
+            $client->messages->create($receiver_number,[
+                'from' => $twilio_number,
+                'body' => $message
+            ]);
+            return redirect()->back();
+        } catch (Exception $e){
+            
+        }
     }
 }
